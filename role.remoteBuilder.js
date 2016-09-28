62f8124a6883bd72277af2620d335aeeb81cd3e9
temik911 <zakharchukart@gmail.com>
2016-09-29 01:52:00 +0300

    harvest from remote rooms

--- role.remoteBuilder.js
+++ role.remoteBuilder.js
@@ -33,8 +33,6 @@ module.exports = {
                     });
                 }
 
-                targets.sort((a, b) => a.hits - b.hits);
-
                 if (targets.length > 0) {
                     if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(targets[0]);

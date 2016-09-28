9563f602c0e57e73b598323bc0386c272b75288a
temik911 <zakharchukart@gmail.com>
2016-09-29 01:59:23 +0300

    harvest from remote rooms

--- role.remoteBuilder.js
+++ role.remoteBuilder.js
@@ -15,30 +15,44 @@ module.exports = {
 
         if (creep.room == flag.room) {
             if (creep.memory.needEnergy == true) {
-                harvestUtils.withdrawFromContainer(creep)
+                harvestUtils.withdrawFromContainer(creep);
 
                 if (creep.carry.energy == creep.carryCapacity) {
                     creep.memory.needEnergy = false;
                 }
             } else {
-                var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: structure => structure.structureType == STRUCTURE_ROAD &&
-                    structure.hits < structure.hitsMax / 2
-                });
-
-                if (targets.length == 0) {
-                    targets = creep.room.find(FIND_STRUCTURES, {
-                        filter: structure => structure.structureType == STRUCTURE_CONTAINER &&
+                if (creep.memory.targetId == false) {
+                    var targets = creep.room.find(FIND_STRUCTURES, {
+                        filter: structure => structure.structureType == STRUCTURE_ROAD &&
                         structure.hits < structure.hitsMax / 2
                     });
+
+                    if (targets.length == 0) {
+                        targets = creep.room.find(FIND_STRUCTURES, {
+                            filter: structure => structure.structureType == STRUCTURE_CONTAINER &&
+                            structure.hits < structure.hitsMax / 2
+                        });
+                    }
+
+                    targets.sort((a, b) => a.hits - b.hits);
+
+                    if (targets.length > 0) {
+                        creep.memory.targetId = targets[0].id;
+                    }
                 }
 
-                if (targets.length > 0) {
-                    if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(targets[0]);
+                var target;
+                if (creep.memory.targetId != false) {
+                    target = Game.getObjectById(creep.memory.targetId);
+                    if (creep.repair(target) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                    }
+
+                    if (target.hits > target.hitsMax * 0.75) {
+                        creep.memory.targetId = false;
                     }
                 } else {
-                    var target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+                    target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
                     if (target) {
                         if (creep.build(target) == ERR_NOT_IN_RANGE) {
                             creep.moveTo(target);
